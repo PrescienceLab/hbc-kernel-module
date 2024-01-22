beb16391f85028aa3e35b9c05a91674f9a79e389
Nadharm <nadharmdhiantravan@gmail.com>
2024-01-22 22:04:36 +0000

    moved to new repo

---
+++ README.md
@@ -0,0 +1,8 @@
+# Heartbeat Kernel Module
+
+Must be tested on: `Linux 5.15.0-73-generic (Ubuntu 22.04.3 LTS)`
+
+To build and insert module:
+```
+$ sudo ./build-and-insmod.sh
+```
---
+++ build-and-insmod.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+make -C src/
+mv src/heartbeat_dev.ko ./
+
+MOD=${1:-./heartbeat_dev.ko}
+
+insmod $MOD \
+    hb_error_entry=0x$(grep ' error_entry' /proc/kallsyms | cut -d' ' -f1) \
+    hb_error_return=0x$(grep ' error_return' /proc/kallsyms | cut -d' ' -f1)
+
+mknod /dev/heartbeat c 239 0
+chmod 777 /dev/heartbeat
